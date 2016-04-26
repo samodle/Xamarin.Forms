d2e5f6a72dc96411a346e6cc2b5e1fe8f0a5bada
smodle <odle@simulationdynamics.com>
2016-04-26 09:40:28 -0400

    created project

---
+++ YahooStock/Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter Stock Symbol...");
+
+
+
+        }
+
+        private void writeIntroToConsole()
+        {
+       /*     Console.WriteLine("   ____ _             _ ___              _");
+            Console.WriteLine(" / ___ || | _ ___ ___| | __ / _ \ _ _  ___ | | _ ___ ___ ");
+            Console.WriteLine("  \___ \| __ / _ \ / __ | |/ / | | | | | | |/ _ \| __ / _ \/ __ | ");
+            Console.WriteLine("    ___) | || (_) | (__ |   <  | | _ | | | _ | | (_) | || __ /\__ \");
+            Console.WriteLine("| ____ / \__\___ / \___ | _ |\_\  \__\_\\__,_ |\___ / \__\___ || ___ / ");
+*/
+
+        }
+    }
+}
