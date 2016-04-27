db351981b917decc05de1e1868e94b8c872e3c74
smodle <odle@simulationdynamics.com>
2016-04-27 11:40:54 -0400

    added shared project

---
+++ YahooStock/StockQuotes/StockQuotes/RawData.cs
@@ -0,0 +1,139 @@
+﻿//using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Analytics
+{
+
+    public static class StockReporting
+    {
+
+        public static void STOCK_ShowMeTheMoney(string Name, string Price, string Delta, string sTime, string sDate)
+        {
+            GetQuoteLatest(Name, ref Price, ref Delta, ref sTime, ref sDate);
+        }
+
+        public async static Task<string> GetQuoteLatest(string pstrSymbol)
+        {
+            string strURL = null;
+            string strBuffer = null;
+
+            //Creates the request URL for Yahoo
+            strURL = "http://quote.yahoo.com/d/quotes.csv?" + "s=" + pstrSymbol + "&d=t" + "&f=sl1d1t1c1ohgvj1pp2wern";
+
+            strBuffer = RequestWebData(strURL);
+
+            return strBuffer;
+            //Loop through the lines returned and transform it to a XML string
+            System.Text.StringBuilder strReturn = new System.Text.StringBuilder();
+            //  strReturn.Append("<StockQuoteLatest>" & Environment.NewLine)
+
+
+
+            /*
+            foreach (string strLine in strBuffer.Split(ControlChars.Lf))
+            {
+                if (strLine.Length > 0)
+                {
+                    //   strReturn.Append(TransformLatestLine(strLine) & Environment.NewLine)
+                }
+            }
+            //    strReturn.Append("</StockQuoteLatest>" & Environment.NewLine)
+            */
+
+
+            return strReturn.ToString();
+        }
+
+        private static void GetQuoteLatest(string pstrSymbol, ref string Price, ref string Delta, ref string sDate, ref string sTime)
+        {
+            string strURL = null;
+            string strBuffer = null;
+            //  Dim WE_HAVE_INTERNET As Boolean = True
+            //Creates the request URL for Yahoo
+            strURL = "http://quote.yahoo.com/d/quotes.csv?" + "s=" + pstrSymbol + "&d=t" + "&f=sl1d1t1c1ohgvj1pp2wern";
+            try
+            {
+                strBuffer = RequestWebData(strURL);
+            }
+            catch (Exception ex)
+            {
+                strBuffer = "";
+                Price = "";
+                Delta = "";
+                sDate = "";
+                sTime = "";
+                //  WE_HAVE_INTERNET = False
+                return;
+            }
+            //Loop through the lines returned and transform it to a XML string
+            //   If WE_HAVE_INTERNET Then
+
+/* commented out because control chars?
+            foreach (string strLine in strBuffer.Split(ControlChars.Lf))
+            {
+                if (strLine.Length > 0)
+                {
+                    TransformLatestLine(strLine, ref Price, ref Delta, ref sDate, ref sTime);
+                }
+            }
+*/
+
+
+            //  End If
+        }
+
+
+        private static string TransformLatestLine(string pstrLine, ref string Price, ref string Delta, ref string sDate, ref string sTime)
+        {
+            string[] arrLine = null;
+            System.Text.StringBuilder strXML = new System.Text.StringBuilder();
+
+            arrLine = pstrLine.Split(',');
+
+            //  Return (arrLine[1] & vbCrLf & arrLine[2].Replace(Chr(34), "") & vbCrLf & arrLine(3).Replace(Chr(34), ""))
+            try
+            {
+                Price = arrLine[1];
+                Delta = arrLine[4];
+                sTime = arrLine[3]; //C# removal    .Replace((char)34, "");
+                sDate = arrLine[2]; //C# removal   .Replace((char)34, "");
+            }
+            catch (Exception ex)
+            {
+                //oops...no internet?
+            }
+            return "";
+        }
+
+        private static string RequestWebData(string pstrURL)
+        {
+            WebRequest objWReq = default(WebRequest);
+            WebResponse objWResp = default(WebResponse);
+            string strBuffer = null;
+
+            //Contact the website
+            objWReq = HttpWebRequest.Create(pstrURL);
+            objWResp = objWReq.GetResponse();
+
+            //Read the answer from the Web site and store it into a stream
+            StreamReader objSR = default(StreamReader);
+            objSR = new StreamReader(objWResp.GetResponseStream());
+            strBuffer = objSR.ReadToEnd();
+            objSR.Close();
+
+            objWResp.Close();
+
+            return strBuffer;
+        }
+        //End Class
+    }
+
+}
---
+++ YahooStock/StockQuotes/StockQuotes/StringTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Analytics
+{
+    public static class StringHelper
+    {
+        public static List<string> splitAtCommas(string rawString)
+        {
+            string str = string.Join("", rawString.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+            return str.Split(',').ToList<string>();
+        }
+
+    }
+}
