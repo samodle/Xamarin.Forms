bf5bb14d27239c93e1683de8a042c0a074d93ea0
smodle <odle@simulationdynamics.com>
2016-04-27 13:31:00 -0400

    working on android

--- YahooStock/StockQuotes/StockQuotes/App.cs
+++ YahooStock/StockQuotes/StockQuotes/App.cs
@@ -9,25 +9,20 @@ namespace StockQuotes
 {
 	public class App : Application
 	{
-		public App ()
-		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-		}
 
-		protected override void OnStart ()
+            public App()
+            {
+                MainPage = new MainPage();
+            }
+
+  
+
+        protected override void OnStart ()
 		{
 			// Handle when your app starts
+
+
+
 		}
 
 		protected override void OnSleep ()
---
+++ YahooStock/StockQuotes/StockQuotes/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analytics;
+
+using Xamarin.Forms;
+
+namespace StockQuotes
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent();
+            goButton.Clicked += GetQuoteButtonClicked;
+		}
+
+
+        async void GetQuoteButtonClicked(object sender, EventArgs e)
+        {
+
+            string line = "";
+            List<string> stockSymbols;
+            //  Task<string> quoteTask; 
+            string quote;
+
+ 
+            line = quoteBox.Text;//THIS NEEDS TO BE TEXTBOX INPUT Console.ReadLine();
+
+
+                stockSymbols = StringHelper.splitAtCommas(line);
+                quote = await StockReporting.GetQuoteLatest(line);
+
+            statusText.Text = quote;
+            }
+
+
+    }
+}
