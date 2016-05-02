e94339adddec984ce2b891ce8d7df037504e4002
smodle <odle@simulationdynamics.com>
2016-05-02 13:05:11 -0400

    added grid & button templates

---
+++ AnimationDemo/GridDemo/GridDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GridDemo
+{
+  /*  public static class Constants
+    {
+        public const double numRows = 7;
+        public const double numCols = 7;
+    }  */
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        
+
+        }
+
+
+
+    }
+}
