2b182ccc6c0fc0fcf6168cba0e0965c3e30733a5
smodle <odle@simulationdynamics.com>
2016-05-02 21:47:42 -0400

    grid buttons

--- AnimationDemo/GridDemo/GridDemo/MainPage.xaml.cs
+++ AnimationDemo/GridDemo/GridDemo/MainPage.xaml.cs
@@ -15,10 +15,35 @@ namespace GridDemo
     }  */
     public partial class MainPage : ContentPage
     {
+        public int LabelGridRow = 1;
+
+        public int LabelRow
+        {
+            get
+            {
+                return LabelGridRow;
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
-        
+            GoButton.Clicked += StartAnimation;
+            StopButton.Clicked += EndAnimation;
+        }
+
+        public void StartAnimation(object a, EventArgs e)
+        {
+            LabelGridRow = 3;
+
+            //  MainGrid.Children.Clear(); //this clears everything
+
+         //   Label1.AnchorX = 1;
+         //   Label1.AnchorY = 1;
+        }
+
+        public void EndAnimation(object a, EventArgs e)
+        {
 
         }
 
